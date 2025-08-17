b8c266c0895331cdc7dc5b70db364f04eecc109f
Elio Villalobos <elioevp@gmail.com>
2025-08-17 01:16:13 -0400

    update2 function

--- function_app.py
+++ function_app.py
@@ -5,14 +5,5 @@ import logging
 
 app = func.FunctionApp()
 
-# Add a simple HTTP trigger for testing
-@app.route(route="hello")
-def http_trigger_hello(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-    return func.HttpResponse(
-        "Hello, world!",
-        status_code=200
-    )
-
 from receipt_processor import blueprint as receipt_blueprint
 app.register_blueprint(receipt_blueprint)
